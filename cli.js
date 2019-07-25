29988f9c901eeb568c948fab021efbb7a0c3a7e7
Peter deHaan <peter@deseloper.com>
2019-07-25 16:47:00 -0700

    Refactor and update shots

--- cli.js
+++ cli.js
@@ -1,52 +1,71 @@
+const fs = require("fs");
 const path = require("path");
 
 const pontoonql = require("pontoonql");
 const puppeteer = require("puppeteer");
 const devices = require("puppeteer/DeviceDescriptors");
 
+const DEFAULTS = {
+  deviceName: "iPhone 8 Plus",
+  viewportHeight: 2500,
+  outDir: path.join(".", "shots"),
+  pontoonProject: "firefox-monitor-website",
+  pontoonMinTranslations: 80
+};
+
 main().catch(err => {
   console.error(err.message);
   process.exit(1);
 });
 
-async function main() {
-  let locales = await pontoonql("firefox-monitor-website", 80);
-  locales = locales.map(l => l.locale.code).sort();
-
-  for (const locale of locales) {
-    await scrapeLocale("https://monitor.firefox.com/", locale);
-  }
-  console.log("finished...");
-  process.exit(0);
-}
-
-async function scrapeLocale(
-  href,
-  locale = "en",
-  deviceName = "iPhone 8 Plus",
-  viewportHeight = 4000
+async function main(
+  deviceName = DEFAULTS.deviceName,
+  viewportHeight = DEFAULTS.viewportHeight
 ) {
-  const filename = path.join(
-    __dirname,
-    "shots",
-    `${locale}-${deviceName.replace(/\s+/g, "_")}.png`
-  );
-
+  const locales = await getLocales();
+  const deviceFilename = deviceName.replace(/\s+/g, "_");
   const mobileDevice = devices[deviceName];
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.emulate(mobileDevice);
   const viewport = page.viewport();
   viewport.height = viewportHeight;
   await page.setViewport(viewport);
+
+  const files = [];
+  for (const locale of locales) {
+    const filename = path.join(
+      DEFAULTS.outDir,
+      `${locale}-${deviceFilename}.png`
+    );
+    // await scrapeLocale(page, "https://monitor.firefox.com/", locale, filename);
+    files.push({ locale, filename });
+  }
+  console.log("finished...");
+  const output = files.reduce((prev, curr) => {
+    prev += `## ${curr.locale}\n![](${curr.filename})\n\n`;
+    return prev;
+  }, "");
+  fs.writeFileSync(
+    path.join(DEFAULTS.outDir, "README.md"),
+    output.toString().trim() + "\n"
+  );
+  process.exit(0);
+}
+
+async function getLocales(pontoonSlug = DEFAULTS.pontoonProject) {
+  const locales = await pontoonql(pontoonSlug, DEFAULTS.pontoonMinTranslations);
+  return locales.map(l => l.locale.code).sort();
+}
+
+async function scrapeLocale(page, href, locale = "en", filename) {
   await page.setExtraHTTPHeaders({ "Accept-Language": locale });
-  await page.goto(href);
+  await page.goto(href, { waitUntil: "networkidle0" });
   await page.evaluate(() =>
     document.querySelector(".latest-breach-headline-wrapper").scrollIntoView()
   );
-  await sleep(3000);
+  await sleep(2000);
   await page.screenshot({ path: filename });
-  console.log("done", filename, viewport);
   return;
 }
 
---
+++ shots/README.md
@@ -0,0 +1,89 @@
+## cak
+![](shots/cak-iPhone_8_Plus.png)
+
+## cs
+![](shots/cs-iPhone_8_Plus.png)
+
+## cy
+![](shots/cy-iPhone_8_Plus.png)
+
+## da
+![](shots/da-iPhone_8_Plus.png)
+
+## de
+![](shots/de-iPhone_8_Plus.png)
+
+## en-CA
+![](shots/en-CA-iPhone_8_Plus.png)
+
+## en-GB
+![](shots/en-GB-iPhone_8_Plus.png)
+
+## es-AR
+![](shots/es-AR-iPhone_8_Plus.png)
+
+## es-CL
+![](shots/es-CL-iPhone_8_Plus.png)
+
+## es-ES
+![](shots/es-ES-iPhone_8_Plus.png)
+
+## es-MX
+![](shots/es-MX-iPhone_8_Plus.png)
+
+## fr
+![](shots/fr-iPhone_8_Plus.png)
+
+## fy-NL
+![](shots/fy-NL-iPhone_8_Plus.png)
+
+## hu
+![](shots/hu-iPhone_8_Plus.png)
+
+## ia
+![](shots/ia-iPhone_8_Plus.png)
+
+## id
+![](shots/id-iPhone_8_Plus.png)
+
+## it
+![](shots/it-iPhone_8_Plus.png)
+
+## kab
+![](shots/kab-iPhone_8_Plus.png)
+
+## nl
+![](shots/nl-iPhone_8_Plus.png)
+
+## pt-BR
+![](shots/pt-BR-iPhone_8_Plus.png)
+
+## pt-PT
+![](shots/pt-PT-iPhone_8_Plus.png)
+
+## ro
+![](shots/ro-iPhone_8_Plus.png)
+
+## ru
+![](shots/ru-iPhone_8_Plus.png)
+
+## sk
+![](shots/sk-iPhone_8_Plus.png)
+
+## sl
+![](shots/sl-iPhone_8_Plus.png)
+
+## sv-SE
+![](shots/sv-SE-iPhone_8_Plus.png)
+
+## tr
+![](shots/tr-iPhone_8_Plus.png)
+
+## vi
+![](shots/vi-iPhone_8_Plus.png)
+
+## zh-CN
+![](shots/zh-CN-iPhone_8_Plus.png)
+
+## zh-TW
+![](shots/zh-TW-iPhone_8_Plus.png)
